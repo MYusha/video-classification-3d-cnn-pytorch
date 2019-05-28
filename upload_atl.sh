510fe3600bb4d3040a6d98986c7bdec62654153b
Yusha Liu <yushal@andrew.cmu.edu>
2019-05-28 12:32:51 -0400

    clean

--- upload_atl.sh
+++
@@ -1,9 +0,0 @@
-# scp ./run_extractdeep.sh atl:~/video_classification_playground/video-classification-3d-cnn-pytorch/
-# scp ./run_video_feature_from_clips.sh atl:~/video_classification_playground/video-classification-3d-cnn-pytorch/
-# scp -r ./subset_labels atl:~/video_classification_playground/video-classification-3d-cnn-pytorch/
-# scp ./main.py atl:~/video_classification_playground/video-classification-3d-cnn-pytorch/
-scp ./extract_feature.py atl:~/video_classification_playground/video-classification-3d-cnn-pytorch/
-scp ./dataset.py atl:~/video_classification_playground/video-classification-3d-cnn-pytorch/
-scp ./classify.py atl:~/video_classification_playground/video-classification-3d-cnn-pytorch/
-# scp ./opts.py atl:~/video_classification_playground/video-classification-3d-cnn-pytorch/
-scp ./spatial_transforms.py atl:~/video_classification_playground/video-classification-3d-cnn-pytorch/
