a62817ce8fd46820aabfc91693e3b56326a5dc90
Yusha Liu <yushal@andrew.cmu.edu>
2019-06-14 11:46:20 -0400

    update

--- run_extract_module.sh
+++ run_extract_module.sh
@@ -1,16 +1,16 @@
-VIDEO_ROOT='../hmdbdata/hmdb51_npy/'
+VIDEO_ROOT='../ucfdata/ucf_small_jpg/'
 MODEL_DIR='../3D-ResNets-PyTorch/models/resnext-101-kinetics.pth'
 # MODEL_DIR='pretrained_files/epoch300_split1_train/save_300.pth'
 OUT_DIR='video_features/'
-LIST_DIR='subset_labels/small_test_list.txt'
+LIST_DIR='../ucfdata/ucf_small_lists/list_only_train.txt'
 # LIST_DIR='subset_labels/video_list_split1_train.txt'
 # LIST_DIR='video_list'
 if [ ! -d $OUT_DIR ]; then
 	mkdir $OUT_DIR
 fi
-Feature_NAME='small_test_features.json'
+Feature_NAME='ucf11_features_train_xx.json'
 test -f $OUT_DIR$Feature_NAME && rm -r $OUT_DIR$Feature_NAME
-CUDA_VISIBLE_DEVICES=3 \
+CUDA_VISIBLE_DEVICES=1 \
 python extract_feature.py \
 --input $LIST_DIR \
 --video_root $VIDEO_ROOT \
